261061f95dc6e05ed12b22804b625f223163760a
Volodymyr-Volan4ik <wolfaska1@gmail.com>
2024-04-02 00:11:08 +0200

    Home_Work5.4.py

---
+++ Home_Work5.4.py
@@ -0,0 +1,77 @@
+def input_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (KeyError, ValueError, IndexError) as e:
+            return f"Error: {str(e)}"
+    return wrapper
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, args
+
+@input_error
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+@input_error
+def change_contact(args, contacts):
+    name, phone = args
+    if name in contacts:
+        contacts[name] = phone
+        return "Contact updated."
+    else:
+        return "Contact not found."
+    
+@input_error
+def show_phone(args, contacts):
+    name = args[0]
+    if name in contacts:
+        return contacts[name]
+    else:
+        return "Contact not found."
+    
+@input_error
+def show_all(contacts):
+    if contacts:
+        contacts_list = [f"{name}: {phone}" for name, phone in contacts.items()]
+        return '\n'.join(contacts_list)
+    else:
+        return "No contacts saved."
+
+
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+
+        elif command == "hello":
+            print("How can I help you?")
+
+        elif command == "add":
+            print(add_contact(args, contacts))
+
+        elif command == "change":
+            print(change_contact(args, contacts))
+
+        elif command == "phone":
+            print(show_phone(args, contacts))
+
+        elif command == "all":
+            print(show_all(contacts))
+
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
